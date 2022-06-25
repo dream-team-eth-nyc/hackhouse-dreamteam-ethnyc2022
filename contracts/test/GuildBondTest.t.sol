aa4e7db953dc616750509047ee4e7d552b4d5f4e
Tanzeela Khan <tanzeela.khan@reddit.com>
2022-06-25 14:08:48 -0400

    GuildBond.sol + Test

---
+++ contracts/test/GuildBondTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import "contracts/guild-manager/GuildBond.sol";
+
+import "forge-std/Test.sol";
+
+contract GuildBondTest is Test {
+    GuildBond manager;
+
+    function setUp() public {
+        // deploy singleton
+        manager = new GuildBond();
+    }
+
+    function testSafeMintWithValidSender() public {
+        assertEq(manager.safeCounterCurrent(), 0);
+        address toAddress = address(12);
+        manager.safeMint(toAddress);
+        assertEq(manager.safeCounterCurrent(), 1);
+    }
+
+    function testSafeMintWithInvalidSender() public {
+        assertEq(manager.safeCounterCurrent(), 0);
+        address toAddress = address(0);
+        try manager.safeMint(toAddress) {
+            fail();
+        } catch {}
+    }
+}
