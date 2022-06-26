4881173468aac97352554569988674800808dddd
npasquie <nicpas@live.fr>
2022-06-26 06:20:29 -0400

    apply update contracts

--- frontend/src/components/ViewPendingInvitationsModal/ViewPendingInvitationsModal.tsx
+++ frontend/src/components/ViewPendingInvitationsModal/ViewPendingInvitationsModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Button,
   Heading,
@@ -11,23 +11,66 @@ import {
   ModalHeader,
   ModalOverlay,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import GuildCard from "../GuildCard/GuildCard";
+import {useAccount, useContractWrite} from 'wagmi';
+import guildBondAbi from '../../abis/GuildBond.json';
 
 type ViewPendingInvitationsModalProps = {
   isOpen: boolean;
   onClose: () => void;
+  onAdd: () => void;
 };
 
 const ViewPendingInvitationsModal: React.FC<
   ViewPendingInvitationsModalProps
-> = ({ isOpen, onClose }) =>
-  isOpen ? (
+> = ({ isOpen, onClose, onAdd }) => {
+  const toast = useToast();
+  const { data } = useAccount();
+  const { isLoading, writeAsync, data: writeData, error: writeError } = useContractWrite(
+    {
+      addressOrName: '0x492B76c462B1060903A9094557ebc8fA69789dcd',
+      contractInterface: guildBondAbi,
+    },
+    'emitSoulBond',
+  );
+
+  useEffect(() => {
+    if (writeData) {
+      toast({
+        title: 'Accepted Invitation!',
+        status: 'success'
+      })
+      onClose();
+    }
+  }, [writeData])
+
+  useEffect(() => {
+    if (writeError) {
+      toast({
+        title: 'Error accepting invitation',
+        description: writeError.message,
+        status: 'error'
+      })
+      onAdd();
+      onClose();
+    }
+  }, [writeError])
+
+
+  const onAccept = async () => {
+    await  writeAsync({
+      args: [data?.address]
+    })
+  };
+
+  return isOpen ? (
     <Modal isOpen={isOpen} onClose={onClose} size="2xl">
-      <ModalOverlay />
+      <ModalOverlay/>
       <ModalContent bgColor="gray.900">
-        <ModalHeader />
-        <ModalCloseButton />
+        <ModalHeader/>
+        <ModalCloseButton/>
         <ModalBody textAlign="center">
           <Heading fontSize="2xl">Pending Guild Invitations</Heading>
           <Text mt={8} color="gray.400">
@@ -41,15 +84,15 @@ const ViewPendingInvitationsModal: React.FC<
               {
                 name: "Bored Ape",
               },
-              { name: "Interesting Iguana" },
+              {name: "Interesting Iguana"},
             ]}
             numNfts={12}
             numGames={8}
-            games={["Axie Infinity", "Eve Online", "Ember Swords"]}
+            games={["Snook", "Eve Online"]}
             bgColor="gray.800"
           >
             <VStack>
-              <Button colorScheme="green">Accept</Button>
+              <Button colorScheme="green" isLoading={isLoading} onClick={onAccept}>Accept</Button>
               <Button colorScheme="red">Reject</Button>
             </VStack>
           </GuildCard>
@@ -63,5 +106,6 @@ const ViewPendingInvitationsModal: React.FC<
       </ModalContent>
     </Modal>
   ) : null;
+};
 
 export default ViewPendingInvitationsModal;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,7 +9,7 @@ import {
   Grid,
 } from "@chakra-ui/react";
 import { AiOutlinePlus } from "react-icons/ai";
-import React from "react";
+import React, { useState } from "react";
 import GuildCard from "../components/GuildCard/GuildCard";
 import ViewPendingInvitationsModal from "../components/ViewPendingInvitationsModal";
 import GuildModal from "../components/GuildModal";
@@ -26,10 +26,51 @@ const Home: React.FC = () => {
   } = useDisclosure();
   const { data } = useAccount();
 
+  const [guilds, setGuilds] = useState([{
+    image: `${process.env.PUBLIC_URL}/Avatar1.png` as any,
+    name: "Dragon Guild",
+    address: "0x66C692026043a4E2E3276dceB7Fa476167CA0C09",
+    members: [
+      {
+        name: "Rabid Melon",
+        image: 'https://forkast.news/wp-content/uploads/2022/03/NFT-Avatar.png'
+      },
+      {
+        name: "Crazy Quail",
+        image: 'http://jingculturecommerce.com/wp-content/uploads/2021/11/rtfkt-murakami-clone-x-4-1024x682.jpg'
+      },
+    ],
+    numNfts: 1,
+    numGames: 1,
+    games: ["Snook"],
+  }
+  ])
+
+  const handleAdd = () => {
+    setGuilds(p => [...p, {
+      image: '',
+      name: "Boar Guild",
+      address: "0x66C692026043a4E2E3276dceB7Fa476167CA0C09",
+      members: [
+        {
+          name: "Bored Ape",
+          image: '',
+        },
+        {
+          name: "Interesting Iguana",
+          image: '',
+        },
+      ],
+      numNfts: 12,
+      numGames: 8,
+      games: ["Snook", "Eve Online"],
+    }])
+  }
+
   return (
     <>
       {guildOpen && <GuildModal isOpen={guildOpen} onClose={guildOnClose} />}
-      <ViewPendingInvitationsModal isOpen={isOpen} onClose={onClose} />
+      <ViewPendingInvitationsModal isOpen={isOpen} onClose={onClose} onAdd={handleAdd} />
       {data ? (
         <Container>
           <HStack alignItems="center" justifyContent="space-between">
@@ -41,7 +82,7 @@ const Home: React.FC = () => {
                 cursor="pointer"
                 onClick={onOpen}
               >
-                (2 pending)
+                (1 pending)
               </Text>
             </HStack>
             <Button
@@ -54,21 +95,12 @@ const Home: React.FC = () => {
             </Button>
           </HStack>
           <SimpleGrid columns={2} spacing={8} mt={8}>
-            <Link to="/guilds/test">
-              <GuildCard
-                name="Dragon Guild"
-                address="0x66C692026043a4E2E3276dceB7Fa476167CA0C09"
-                members={[
-                  {
-                    name: "Test",
-                  },
-                  { name: "Test 2" },
-                ]}
-                numNfts={12}
-                numGames={8}
-                games={["Axie Infinity", "Eve Online", "Ember Swords"]}
-              />
-            </Link>
+            {guilds.map((props: any) =>
+              <Link to="/guilds/test" key={props.name + props.image}>
+                <GuildCard
+                  {...props}
+                />
+              </Link>)}
           </SimpleGrid>
         </Container>
       ) : (
